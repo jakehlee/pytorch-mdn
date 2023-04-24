80093b02199262b1f3515050768df3142e9b24e2
Jake Lee <jake.h.lee@jpl.nasa.gov>
2023-04-24 21:50:20 +0000

    Added 2d MDN

--- mdn/mdn.py
+++ mdn/mdn.py
@@ -57,6 +57,53 @@ class MDN(nn.Module):
         return pi, sigma, mu
 
 
+class MDN2D(nn.Module):
+    """A 2D mixture density network layer
+
+    The input maps to the parameters of a MoG probability distribution, where
+    each Gaussian has O dimensions and diagonal covariance.
+
+    Arguments:
+        in_features (int): the number of channels in the input
+        out_features (int): the number of channels in the output
+        num_gaussians (int): the number of Gaussians per output channel
+
+    Input:
+        minibatch(BxCxHxW): B is the batch size and C is the number of channels.
+
+    Output:
+        (pi, sigma, mu) (BxG, BxGxCxHxW, BxGxCxHxW): B is the batch size, G is the
+            number of Gaussians, and C is the number of channels for each
+            Gaussian. Pi is a multinomial distribution of the Gaussians. Sigma
+            is the standard deviation of each Gaussian. Mu is the mean of each
+            Gaussian.
+    """
+
+    def __init__(self, in_features, out_features, num_gaussians):
+        super(MDN2D, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.num_gaussians = num_gaussians
+
+        self.pi = nn.Sequential(
+            nn.Conv2d(in_features, num_gaussians, kernel_size=(1,1)),
+            nn.Softmax(dim=1)
+        )
+
+        self.sigma = nn.Conv2d(in_features,
+                               out_features * num_gaussians,
+                               kernel_size=(1,1))
+
+        self.mu = nn.Conv2d(in_features,
+                            out_features * num_gaussians,
+                            kernel_size=(1,1))
+
+    def forward(self, minibatch):
+        pi = self.pi(minibatch)
+        sigma = torch.exp(self.sigma(minibatch))
+        mu = self.mu(minibatch)
+        return pi, sigma, mu
+
 def gaussian_probability(sigma, mu, target):
     """Returns the probability of `target` given MoG parameters `sigma` and `mu`.
 
@@ -77,8 +124,62 @@ def gaussian_probability(sigma, mu, target):
     ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((target - mu) / sigma)**2) / sigma
     return torch.prod(ret, 2)
 
+def gaussian_probability_nan(sigma, mu, target):
+    """Returns the probability of `target` given MoG parameters `sigma` and `mu`.
+
+    Arguments:
+        sigma (BxGxO): The standard deviation of the Gaussians. B is the batch
+            size, G is the number of Gaussians, and O is the number of
+            dimensions per Gaussian.
+        mu (BxGxO): The means of the Gaussians. B is the batch size, G is the
+            number of Gaussians, and O is the number of dimensions per Gaussian.
+        target (BxI): A batch of target. B is the batch size and I is the number of
+            input dimensions.
+
+    Returns:
+        probabilities (BxG): The probability of each point in the probability
+            of the distribution in the corresponding sigma/mu index.
+    """
+    target = target.unsqueeze(1).expand_as(sigma)
+    ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((target - mu) / sigma)**2) / sigma
+
+    # Some target values are NaN due to sparse labels.
+    # Any loss calculated there should just not contribute to the product
+    ret = torch.nan_to_num(ret, nan=1)
+    return torch.prod(ret, 2)
+
+def gaussian_probability_2d(sigma, mu, target):
+    """Returns the probability of `target` given MoG parameters `sigma` and `mu`.
+
+    Arguments:
+        sigma (BxGxO): The standard deviation of the Gaussians. B is the batch
+            size, G is the number of Gaussians, and O is the number of
+            dimensions per Gaussian.
+        mu (BxGxO): The means of the Gaussians. B is the batch size, G is the
+            number of Gaussians, and O is the number of dimensions per Gaussian.
+        target (BxI): A batch of target. B is the batch size and I is the number of
+            input dimensions.
+
+    Returns:
+        probabilities (BxG): The probability of each point in the probability
+            of the distribution in the corresponding sigma/mu index.
+    """
+    #target = target.unsqueeze(1).expand_as(sigma)
+    ret = ONEOVERSQRT2PI * torch.exp(-0.5 * ((target - mu) / sigma)**2) / sigma
+    return torch.prod(ret, 2)
 
 def mdn_loss(pi, sigma, mu, target):
+    """Calculates the error, given the MoG parameters and the target
+
+    The loss is the negative log likelihood of the data given the MoG
+    parameters.
+    """
+    prob = pi * gaussian_probability(sigma, mu, target)
+    prob[prob<=1e-30] = 1e-30
+    nll = -torch.log(torch.sum(prob, dim=1))
+    return torch.mean(nll)
+
+def mdn_loss_nan(pi, sigma, mu, target):
     """Calculates the error, given the MoG parameters and the target
 
     The loss is the negative log likelihood of the data given the MoG
@@ -88,6 +189,15 @@ def mdn_loss(pi, sigma, mu, target):
     nll = -torch.log(torch.sum(prob, dim=1))
     return torch.mean(nll)
 
+def mdn_loss_2d(pi, sigma, mu, target):
+    """Calculates the error, given the MoG parameters and the target
+
+    The loss is the negative log likelihood of the data given the MoG
+    parameters.
+    """
+    prob = pi * gaussian_probability_2d(sigma, mu, target)
+    nll = -torch.log(torch.sum(prob, dim=1))
+    return torch.mean(nll)
 
 def sample(pi, sigma, mu):
     """Draw samples from a MoG.
@@ -102,3 +212,16 @@ def sample(pi, sigma, mu):
     variance_samples = sigma.gather(1, pis).detach().squeeze()
     mean_samples = mu.detach().gather(1, pis).squeeze()
     return (gaussian_noise * variance_samples + mean_samples).transpose(0, 1)
+
+def sample_2d(pi, sigma, mu):
+    """Draw samples from a MoG.
+    """
+    # Choose which gaussian we'll sample from
+    pis = Categorical(torch.flatten(pi, start_dim=2).transpose(1,2)).sample().reshape(pi.shape[0], 1, pi.shape[2], pi.shape[3])
+    # Choose a random sample, one randn for batch X output dims
+    # Do a (output dims)X(batch size) tensor here, so the broadcast works in
+    # the next step, but we have to transpose back.
+    gaussian_noise = torch.randn(pis.shape, requires_grad=False)
+    variance_samples = sigma.gather(1, pis).detach().to('cpu')
+    mean_samples = mu.gather(1, pis).detach().to('cpu')
+    return (gaussian_noise * variance_samples + mean_samples)
